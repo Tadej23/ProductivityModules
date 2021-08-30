3637c5ff4bb2418e330e88f8453b9e7ada2f7677
Tadej23 <47983987+Tadej23@users.noreply.github.com>
2021-08-30 20:43:49 +0200

    Tables

--- DataModel/CitationSources.cs
+++ DataModel/CitationSources.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataModel
 {
-    class CitationSources
+    [Table("tblCitationSources")]
+    public class CitationSources
     {
     }
 }
--- DataModel/Citations.cs
+++ DataModel/Citations.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataModel
 {
-    class Citations
+    [Table ("tblCitations")]
+    public class Citations
     {
     }
 }
