d8c96a0d37d29030d5c34a07e894543438a2fd30
Tadej23 <47983987+Tadej23@users.noreply.github.com>
2021-08-30 20:43:01 +0200

    Update ArtWorkAuthor.cs

--- DataModel/ArtWorkAuthor.cs
+++ DataModel/ArtWorkAuthor.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataModel
 {
-    class ArtWorkAuthor
+    [Table("tblArtWorkAuthor")]
+    public class ArtWorkAuthor
     {
     }
 }
