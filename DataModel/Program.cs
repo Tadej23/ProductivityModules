d157366b8457bd15ede9fc6966f62cb003ae1905
Tadej23 <47983987+Tadej23@users.noreply.github.com>
2021-08-31 14:21:39 +0200

    DbSets

--- DataModel/Program.cs
+++ DataModel/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿
+using Microsoft.EntityFrameworkCore;
 
 namespace DataModel
 {
-    class Program
+    public class Program : DbContext
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+       
+        
+        public DbSet<ArtWorkAuthor> ArtWorkAuthor { get; set; }
+        public DbSet<ArtWorks> ArtWorks { get; set; }
+        public DbSet<Citations> Citations{ get; set; }
+        public DbSet<CitationSources> CitationSources { get; set; }
+        public DbSet<PatentAuthor> PatentAuthor { get; set; }
+        public DbSet<Patents> Patents { get; set; }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<PersonProfession> PersonProfession { get; set; }
+        public DbSet<PersonProfiles> PersonProfiles { get; set; }
+        public DbSet<ProfessionCategory> ProfessionCategory { get; set; }
+        public DbSet<Profiles> Profiles { get; set; }
+        public DbSet<ProjectPerson> ProjectPerson { get; set; }
+        public DbSet<ProjectType> ProjectType { get; set; }
+        public DbSet<PublicationAuthor> PublicationAuthor { get; set; }
+        public DbSet<Publications> Publications { get; set; }
+        public DbSet<SameWork> SameWork { get; set; }
+        public DbSet<Projects> ArtWorkAuthor { get; set; }
+
+
+
     }
 }
